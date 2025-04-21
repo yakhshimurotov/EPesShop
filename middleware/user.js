1d04536254bea31db360547c8ff8673d5f564662
Jahongir Yahshimurotov <jahongiryahshimurotov@gmail.com>
2025-04-22 01:01:05 +0500

    EPes-v1.7

--- middleware/user.js
+++ middleware/user.js
@@ -3,7 +3,6 @@ import User from "../models/User.js"
 
 export default async function(req, res, next) {
     if(!req.cookies.token){
-        // res.redirect("/login");
         next();
         return
     };
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
+import moment from "moment";
 export default {
     ifequal(a, b, options) {
-        if (a == b) {
+        if (String(a) === String(b)) {
             return options.fn(this);
         }
         return options.inverse(this);
@@ -8,5 +9,8 @@ export default {
     getFullNameCharacter(name) {
         if (!name) return '';
         return name.charAt(0).toUpperCase();
-    }
+    },
+        formatData(data) {
+            return moment(data).format("YYYY-MMM-DD");
+        }
 };
