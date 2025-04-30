70e3d09c40612c91366bc213fc78f5bced91da2b
Jahongir Yahshimurotov <jahongiryahshimurotov@gmail.com>
2025-04-30 20:52:47 +0500

    fix: renamed User.js to user.js to match import

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ import * as dotenv from "dotenv";
 import flash from "connect-flash";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+import MongoStore from 'connect-mongo';
 
 import varMiddleware from "./middleware/var.js";
 import userMiddleware from "./middleware/user.js";
 import hbsHelper from "./utils/index.js";
-
-// Routers
 import mainRoutes from "./routes/main.js";
 import registerRoutes from "./routes/register.js";
 
 dotenv.config();
-const app = express();
 
-const hbs = create({ defaultLayout: "main", extname: "hbs", helpers: hbsHelper});
+const app = express(); // ⚠️ BU YERDA BIRINCHI BO‘LISHI KERAK
+
+const hbs = create({ defaultLayout: "main", extname: "hbs", helpers: hbsHelper });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
@@ -25,7 +25,22 @@ app.set("views", "./views");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({secret: "Jokhn", resave: false, saveUninitialized: false}));
+
+app.use(session({
+  secret: process.env.JWT_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URL,
+    ttl: 14 * 24 * 60 * 60
+  }),
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
+
 app.use(flash());
 app.use(cookieParser());
 app.use(varMiddleware);
@@ -46,8 +61,8 @@ const appStart = () => {
       console.log(`Server is running ${PORT}`);
     });
   } catch (error) {
-    console.log(error)
-  };
+    console.log(error);
+  }
 };
 
 appStart();
